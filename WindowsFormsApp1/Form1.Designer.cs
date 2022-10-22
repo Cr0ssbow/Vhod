45243e043c446302f0a0dede44298d529fda9fbf
Евгений Закиров <gena-guk@mail.ru>
2022-10-22 13:41:46 +0500

    Спросить как законектить формы

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -219,7 +219,7 @@ namespace WindowsFormsApp1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(550, 378);
+            this.ClientSize = new System.Drawing.Size(554, 386);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
